8993b91557632b87d8d41e56747d7d01d6bfe2da
LaLaSero <yutakagi@lalase.local>
2024-03-23 17:11:33 +0900

    fixed unset

--- srcs/builtins/builtin_unset.c
+++ srcs/builtins/builtin_unset.c
@@ -6,7 +6,7 @@
 /*   By: yutakagi <yutakagi@student.42.jp>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/05 17:59:58 by yutakagi          #+#    #+#             */
-/*   Updated: 2024/03/23 16:34:55 by yutakagi         ###   ########.fr       */
+/*   Updated: 2024/03/23 17:11:04 by yutakagi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,29 @@ static void	_show_unset_error(char *key)
 	write(STDERR_FILENO, "': not a valid identifier\n", 26);
 }
 
+static bool	is_valid_unset_variable(const char *variable_name)
+{
+	int	i;
+
+	if (variable_name == NULL || *variable_name == '\0'
+		|| *variable_name == '=')
+		return (false);
+	if (ft_isdigit((int)*variable_name))
+		return (false);
+	i = 1;
+	while (variable_name[i])
+	{
+		if (variable_name[i] == '=')
+			return (false);
+		else if (!ft_isalpha((int)variable_name[i])
+			&& !ft_isdigit((int)variable_name[i])
+			&& variable_name[i] != '_')
+			return (false);
+		i++;
+	}
+	return (true);
+}
+
 int	builtin_unset(char **argv, t_map *envmap)
 {
 	int	status;
@@ -30,7 +53,7 @@ int	builtin_unset(char **argv, t_map *envmap)
 	i = 0;
 	while (argv[i])
 	{
-		if (is_valid_export_variable(argv[i]) == false)
+		if (is_valid_unset_variable(argv[i]) == false)
 		{
 			status = 1;
 			_show_unset_error(argv[i]);
@@ -39,7 +62,6 @@ int	builtin_unset(char **argv, t_map *envmap)
 		}
 		else if (remove_var(envmap, argv[i]) == FAILURE)
 		{
-			_show_unset_error(argv[i]);
 			status = 0;
 		}
 		else
